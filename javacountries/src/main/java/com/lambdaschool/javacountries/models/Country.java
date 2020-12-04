c32be32f2d3dd404480e924ad33f4c321c522d0b
Christopher Corvo <christophercorvo@gmail.com>
2020-12-03 19:59:41 -0500

    initial commit

---
+++ javacountries/src/main/java/com/lambdaschool/javacountries/models/Country.java
@@ -0,0 +1,97 @@
+// Explanation:
+
+package com.lambdaschool.javacountries.models;
+
+import javax.persistence.*;
+
+// What is this?
+@Entity
+
+// What is this?
+@Table(name = "countries")
+
+public class Country
+{
+    // What is this?
+    @Id
+    // What is this?
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    // These are my table fields
+    private String name;
+    private long population;
+    private long landmasskm2;
+    private int medianage;
+
+    // This is my default constructor - required for Spring Data JPA
+    public Country()
+    {
+        // Default Constructor
+    }
+
+    // Personally made constructor with parameters
+    public Country(
+        String name,
+        long population,
+        long landmasskm2,
+        int medianage)
+    {
+        this.name = name;
+        this.population = population;
+        this.landmasskm2 = landmasskm2;
+        this.medianage = medianage;
+    }
+
+    // These are my Getter and Setter methods for the above fields
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public long getPopulation()
+    {
+        return population;
+    }
+
+    public void setPopulation(long population)
+    {
+        this.population = population;
+    }
+
+    public long getLandmasskm2()
+    {
+        return landmasskm2;
+    }
+
+    public void setLandmasskm2(long landmasskm2)
+    {
+        this.landmasskm2 = landmasskm2;
+    }
+
+    public int getMedianage()
+    {
+        return medianage;
+    }
+
+    public void setMedianage(int medianage)
+    {
+        this.medianage = medianage;
+    }
+
+    // What is this?
+    @Override
+    public String toString()
+    {
+        return "Country{" +
+            "name=" + name +
+            "population=" + population +
+            "landmasskm2=" + landmasskm2 +
+            "medianage=" + medianage +
+            '}';
+    }
+}
